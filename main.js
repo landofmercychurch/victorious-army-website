4c22b04892318084b7f1ad89e1fe8bca08038fe9
landofmercychurch <oluwaseyio710@gmail.com>
2025-09-24 23:45:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+import { initMemorials } from "./memorials.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initMemorials(document.getElementById("memorialsContainer"));
+});
+
+
+import { initSermons } from "./sermons.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initSermons(document.getElementById("sermonsContainer"));
+});
+
+
+import { initPicturePosts } from "./picturePosts.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initPicturePosts(document.getElementById("picturePostsContainer"));
+});
+
+import { initEvents } from "./events.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initEvents(document.getElementById("eventsContainer"));
+});
+
+
+import { initDailyVerse } from "./dailyVerse.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initDailyVerse(document.getElementById("dailyVerseContainer"));
+});
