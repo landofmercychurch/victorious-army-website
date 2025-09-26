b269a9159d9be224a32d5b704e7b8389a81e3d3e
landofmercychurch <oluwaseyio710@gmail.com>
2025-09-26 03:41:39 +0100

    Update commentsPublic.js

--- commentsPublic.js
+++ commentsPublic.js
@@ -2,7 +2,28 @@
 import { api } from "./api.js";
 import { showNotification } from "./config.js";
 
-export async function postGuestComment({ name, content, target_type, target_id }) {
+/**
+ * Post a comment on a sermon
+ */
+export async function postSermonComment({ name = "Guest", content, sermon_id }) {
   if (!content) throw new Error("No content");
-  return api.post("/comments", { name, content, target_type, target_id });
+  try {
+    return await api.post("/comments", { name, content, sermon_id });
+  } catch (err) {
+    showNotification?.("Failed to post sermon comment", "error");
+    throw err;
+  }
+}
+
+/**
+ * Post a comment on a picture post
+ */
+export async function postPictureComment({ name = "Guest", content, post_id }) {
+  if (!content) throw new Error("No content");
+  try {
+    return await api.post("/comments", { name, content, post_id });
+  } catch (err) {
+    showNotification?.("Failed to post post comment", "error");
+    throw err;
+  }
 }
